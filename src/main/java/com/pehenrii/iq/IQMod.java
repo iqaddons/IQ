819f5e6ed5ee24f2b41c13880515f3edc83f4bb4
Pedro Henrique <50496954+pehenrii@users.noreply.github.com>
2026-01-07 06:18:18 -0300

    feat: update build configuration and add logging to IQMod

--- src/main/java/com/pehenrii/iq/IQMod.java
+++ src/main/java/com/pehenrii/iq/IQMod.java
@@ -1,11 +1,18 @@
 package com.pehenrii.iq;
 
+import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.api.ClientModInitializer;
 
+@Slf4j
 public class IQMod implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        log.info("Initializing IQ Mod...");
 
+        log.info("IQ Mod has been initialized!");
+    }
+
+    private void registerTicks() {
     }
 }
