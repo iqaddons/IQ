0127d17e6df9936b2edb2574b2300d052cb4ea5f
Pedro Henrique <50496954+pehenrii@users.noreply.github.com>
2026-02-07 00:16:55 -0300

    feat(kuudra): improve spawn direction alert logic and enhance logging

--- src/client/java/net/iqaddons/mod/features/kuudra/alerts/KuudraDirectionAlertFeature.java
+++ src/client/java/net/iqaddons/mod/features/kuudra/alerts/KuudraDirectionAlertFeature.java
@@ -3,6 +3,7 @@ package net.iqaddons.mod.features.kuudra.alerts;
 import lombok.extern.slf4j.Slf4j;
 import net.iqaddons.mod.config.categories.PhaseFourConfig;
 import net.iqaddons.mod.events.impl.ClientTickEvent;
+import net.iqaddons.mod.events.impl.skyblock.KuudraPhaseChangeEvent;
 import net.iqaddons.mod.features.KuudraFeature;
 import net.iqaddons.mod.state.kuudra.KuudraPhase;
 import net.iqaddons.mod.utils.KuudraLocationUtil;
@@ -10,12 +11,14 @@ import net.iqaddons.mod.utils.MessageUtil;
 import net.minecraft.entity.mob.MagmaCubeEntity;
 import org.jetbrains.annotations.NotNull;
 
+import static net.iqaddons.mod.utils.KuudraLocationUtil.SpawnDirection.UNKNOWN;
+
 @Slf4j
 public class KuudraDirectionAlertFeature extends KuudraFeature {
 
     private static final int CHECK_INTERVAL_TICKS = 2;
 
-    private volatile boolean alertShown = false;
+    private volatile KuudraLocationUtil.SpawnDirection currentDirection = UNKNOWN;
 
     public KuudraDirectionAlertFeature() {
         super(
@@ -28,32 +31,32 @@ public class KuudraDirectionAlertFeature extends KuudraFeature {
 
     @Override
     protected void onKuudraActivate() {
-        alertShown = false;
-
+        currentDirection = null;
         subscribe(ClientTickEvent.class, this::onTick);
     }
 
     @Override
     protected void onKuudraDeactivate() {
-        alertShown = false;
+        currentDirection = null;
     }
 
     private void onTick(@NotNull ClientTickEvent event) {
         if (!event.isInGame()) return;
         if (!event.isNthTick(CHECK_INTERVAL_TICKS)) return;
-        if (alertShown) return;
 
         var optionalKuudra = KuudraLocationUtil.findKuudra();
         if (optionalKuudra.isEmpty()) return;
 
         MagmaCubeEntity kuudra = optionalKuudra.get();
         var direction = KuudraLocationUtil.getSpawnDirection(kuudra);
-        log.debug("Detected Kuudra spawn, direction: {}", direction.getName());
-
-        if (direction != KuudraLocationUtil.SpawnDirection.UNKNOWN) {
+        if (direction != UNKNOWN && direction != currentDirection) {
+            currentDirection = direction;
             MessageUtil.showTitle(direction.getFormattedName(), "", 0, 25, 5);
-            alertShown = true;
-            log.debug("Kuudra spawn alert: {}", direction.getName());
+            log.info("Kuudra spawn direction alert: {} (pos: {}, {}, {})",
+                    direction.getName(),
+                    String.format("%.1f", kuudra.getX()),
+                    String.format("%.1f", kuudra.getY()),
+                    String.format("%.1f", kuudra.getZ()));
         }
     }
 }
