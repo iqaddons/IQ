ba664e883381751bc655e9de1dcc58be0f2556ff
Pedro Henrique <50496954+pehenrii@users.noreply.github.com>
2026-02-09 18:23:40 -0300

    refactor: restructure feature management with lifecycle components

---
+++ src/client/java/net/iqaddons/mod/lifecycle/modules/KuudraModule.java
@@ -0,0 +1,15 @@
+package net.iqaddons.mod.lifecycle.modules;
+
+import net.iqaddons.mod.lifecycle.LifecycleComponent;
+
+public class KuudraModule implements LifecycleComponent {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
---
+++ src/client/java/net/iqaddons/mod/lifecycle/modules/WidgetModule.java
@@ -0,0 +1,28 @@
+package net.iqaddons.mod.lifecycle.modules;
+
+import lombok.Getter;
+import net.iqaddons.mod.features.widgets.*;
+import net.iqaddons.mod.hud.HudManager;
+import net.iqaddons.mod.lifecycle.LifecycleComponent;
+
+public class WidgetModule implements LifecycleComponent {
+
+    private HudManager hudManager;
+
+    @Override
+    public void start() {
+        hudManager = new HudManager();
+        hudManager.initialize();
+
+        hudManager.register(
+                new SupplyTimerWidget(), new BuildProgressWidget(),
+                new CustomSplitsWidget(), new FreshCountdownWidget(),
+                new KuudraHealthWidget(), new FreshersTimerWidget()
+        );
+    }
+
+    @Override
+    public void stop() {
+        hudManager.stop();
+    }
+}
