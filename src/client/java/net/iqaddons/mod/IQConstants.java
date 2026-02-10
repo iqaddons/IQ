d50d1f795a9c2353c3d142084283e5d8a6b5b65b
Pedro Henrique <50496954+pehenrii@users.noreply.github.com>
2026-02-09 22:46:52 -0300

    fix: simplify NoPreAlertFeature by removing unnecessary checks and improving message handling

--- src/client/java/net/iqaddons/mod/IQConstants.java
+++ src/client/java/net/iqaddons/mod/IQConstants.java
@@ -13,4 +13,7 @@ public final class IQConstants {
     public static final Pattern SUPPLY_PATTERN = Pattern.compile("(.+) recovered one of Elle's supplies! \\((\\d)/6\\)");
     public static final Pattern PARTY_FRESH_PATTERN = Pattern.compile("Party > (?:\\[[^]]+] )?(\\w+): (?:\\[IQ] )?FRESH");
     public static final Pattern PROTECT_ELLE_PATTERN = Pattern.compile("Protect Elle\\s*\\((\\d+)%\\)");
+
+    public static final String ELLE_HEAD_OVER_MESSAGE = "[NPC] Elle: Head over to the main platform";
+    public static final String ELLE_NOT_AGAIN_MESSAGE = "[NPC] Elle: Not again!";
 }
