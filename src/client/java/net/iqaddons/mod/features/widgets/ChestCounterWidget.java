b3942eedba7a482f42af060095e09f22c7a7221d
Pedro Henrique <50496954+pehenrii@users.noreply.github.com>
2026-02-12 03:15:15 -0300

    feat: enhance ChestCounterWidget with styled text and improved HudLine handling

--- src/client/java/net/iqaddons/mod/features/widgets/ChestCounterWidget.java
+++ src/client/java/net/iqaddons/mod/features/widgets/ChestCounterWidget.java
@@ -8,17 +8,24 @@ import net.iqaddons.mod.hud.component.HudLine;
 import net.iqaddons.mod.hud.element.HudAnchor;
 import net.iqaddons.mod.hud.element.HudWidget;
 import net.iqaddons.mod.manager.ChestCounterManager;
+import net.minecraft.text.StyleSpriteSource;
+import net.minecraft.text.Text;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
+import static net.iqaddons.mod.IQConstants.IQ_FONT_IDENTIFIER;
+
 @Slf4j
 public class ChestCounterWidget extends HudWidget {
 
     private static final String KUUDRA_ICON = "\uE000";
+    private static final StyleSpriteSource.Font IQ_FONT = new StyleSpriteSource.Font(IQ_FONT_IDENTIFIER);
 
-    private final HudLine line = HudLine.of(KUUDRA_ICON + " §f§l0/60");
+    private final HudLine line = HudLine.of(Text.literal(KUUDRA_ICON)
+            .styled(style -> style.withFont(IQ_FONT))
+            .append(Text.literal(" §f0§7/60")));
 
     public ChestCounterWidget() {
         super("chestCounterWidget", "Chest Counter",
@@ -32,7 +39,10 @@ public class ChestCounterWidget extends HudWidget {
                 () -> ChestCounterManager.get().getChests() > 0 && ChestCounterTrackerFeature.overlayVisible
         );
 
-        setExampleLines(List.of(HudLine.of(KUUDRA_ICON + " §a20§f/60")));
+        setExampleLines(List.of(HudLine.of(Text.literal(KUUDRA_ICON)
+                .styled(style -> style.withFont(IQ_FONT))
+                .append(Text.literal(" §a20§7/60"))))
+        );
     }
 
     @Override
@@ -51,14 +61,19 @@ public class ChestCounterWidget extends HudWidget {
 
     private void updateLine() {
         int chests = ChestCounterManager.get().getChests();
-        line.text(KUUDRA_ICON + " " + getColor(chests) + chests + "§f/" + ChestCounterManager.MAX_CHESTS);
+        line.text(Text.literal(KUUDRA_ICON)
+                .styled(style -> style.withFont(IQ_FONT))
+                .append(Text.literal(String.format(" %s%d§7/%d",
+                        getColor(chests), chests, ChestCounterManager.MAX_CHESTS)))
+        );
+
         markDimensionsDirty();
     }
 
     @Contract(pure = true)
     private @NotNull String getColor(int count) {
-        if (count >= 60) return "§c§l";
-        if (count >= 50) return "§4";
+        if (count >= 60) return "§4§l";
+        if (count >= 50) return "§c";
         if (count >= 40) return "§6";
         if (count >= 30) return "§e";
         if (count >= 20) return "§a";
